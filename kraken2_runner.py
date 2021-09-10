89b90aebda84170198292aee6e480f0b6200e758
wanyuac <wanyuac@126.com>
2021-09-10 12:34:03 +0100

    Improvement

--- kraken2_runner.py
+++ kraken2_runner.py
@@ -9,7 +9,7 @@ Notes:
 
 Copyright (C) 2021 Yu Wan <wanyuac@126.com>
 Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-First version: 5 Aug 2021; the latest update: 6 Aug 2021
+First version: 5 Aug 2021; the latest update: 10 Sep 2021
 """
 
 import os
@@ -27,9 +27,12 @@ def parse_arguments():
     parser.add_argument("--outdir", "-o", dest = "outdir", type = str, required = False, default = "output", help = "Parental output directory")
     parser.add_argument("--ncpus", "-n", dest = "ncpus", type = str, required = False, default = "8", help = "Number of computational cores to be requested (default: 8)")
     parser.add_argument("--mem", "-m", dest = "mem", type = str, required = False, default = "64", help = "Memory size (GB) to be requested (default: 64)")
-    parser.add_argument("--queue", "-q", dest = "queue", type = int, required = False, default = 10, help = "Size of each serial job queue")
+    parser.add_argument("--queue", "-q", dest = "queue", type = int, required = False, default = 10, help = "Size of each serial job queue (Default: 10)")
     parser.add_argument("--scheduler", "-s", dest = "scheduler", type = str, required = False, default = "SGE", help = "Job scheduler (SGE/PBS); default: SGE")
+    parser.add_argument("--env_module", "-e", dest = "env_module", type = str, required = False, default = "anaconda/5.3.1_python3", help = "(Optional) Environmental module to be loaded")
+    parser.add_argument("--conda_env", "-c", dest = "conda_env", type = str, required = False, default = "kraken", help = "(Optional) Conda environment to be loaded")
     parser.add_argument("--debug", "-d", dest = "debug", action = "store_true", help = "Only generate job script but do not submit it")
+
     return parser.parse_args()
 
 
@@ -48,13 +51,15 @@ def main():
         if k == args.queue:  # When the current queue becomes full
             n += 1
             scripts.append(write_job_script(create_job_script({genome : readsets[genome] for genome in queue}, args.db, args.ncpus,\
-                                                              args.mem, args.outdir, args.scheduler), k, n, args.outdir, args.scheduler))  # Append the path of the new script to list 'scripts'
+                                                              args.mem, args.outdir, args.scheduler, args.env_module, args.conda_env),\
+                                                              k, n, args.outdir, args.scheduler))  # Append the path of the new script to list 'scripts'
             queue = list()
             k = 0
     if k > 0:  # When there are remaining tasks in the last queue.
         n += 1
         scripts.append(write_job_script(create_job_script({genome : readsets[genome] for genome in queue}, args.db, args.ncpus,\
-                                                          args.mem, args.outdir, args.scheduler), k, n, args.outdir, args.scheduler))
+                                                          args.mem, args.outdir, args.scheduler, args.env_module, args.conda_env),\
+                                                          k, n, args.outdir, args.scheduler))
     if submit:
         for s in scripts:
             print("Submit job script " + s, file = sys.stdout)
@@ -63,7 +68,7 @@ def main():
     return
 
 
-def create_job_script(readsets, db, ncpus, mem, outdir, scheduler):
+def create_job_script(readsets, db, ncpus, mem, outdir, scheduler, env_module, conda_env):
     outdir = os.path.abspath(outdir)
     if scheduler == "SGE":
         script = f"""#!/bin/bash
@@ -72,14 +77,19 @@ def create_job_script(readsets, db, ncpus, mem, outdir, scheduler):
 #$ -S /bin/bash
 #$ -pe multithread {ncpus}
 #$ -l h_vmem={mem}G
-
+"""
+        if env_module != "":
+            script += f"""
 # Environmental settings
 source $HOME/.bash_profile
 source /etc/profile.d/modules.sh
 module purge
-module load anaconda/5.3.1_python3
-conda activate kraken
-
+module load {env_module}
+"""
+        if conda_env != "":
+            script += f"""conda activate {conda_env}
+"""
+        script +="""
 # Kraken2 jobs"""
     else:  # PBS job script
         script = f"""#!/bin/bash
@@ -87,11 +97,16 @@ conda activate kraken
 #PBS -N kraken
 #PBS -l select=1:ncpus={ncpus}:mem={mem}gb:ompthreads={ncpus}
 #PBS -l walltime=24:00:00
-
+"""
+        if env_module != "":
+            script += """
 # Environmental settings
-module load anaconda3/personal
-source activate kraken
-
+module load {env_module}
+"""
+        if conda_env != "":
+            script += """source activate kraken
+"""
+        script += """
 # Kraken2 jobs"""
 
     genomes = list(readsets.keys())
